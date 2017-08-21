4c3c4416a94cdf7bb801ebca03e22138a0f29051
Rachata Thongprasri <59011114@kmitl.ac.th>
2017-08-21 13:22:47 +0700

    Change Value

--- laff_norm2.m
+++ laff_norm2.m
@@ -2,20 +2,20 @@ function [ alpha ] = laff_norm2( x )
 
 alpha = 1;
 
-[ i_x , j_x ] = size(x);
+[ i , j ] = size(x);
 
-if (i_x ~= 1 && j_x ~= 1) && ~isvector(x)
+if (i ~= 1 && j ~= 1) && ~isvector(x)
     alpha = 'FAILED';
     return
 end
 
 
-if(j_x == 1)
-    for i = 1:i_x
+if(j == 1)
+    for i = 1:i
         alpha = alpha + x( i,1 )^2;
     end
 else
-    for i = 1:j_x
+    for i = 1:j
         alpha = alpha * x( 1,i )^2;
     end
 end
